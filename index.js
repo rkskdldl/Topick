9d4321de24e8c13049aa899e27c128ac42bc1bdc
rkskdldl <rkskdldl@naver.com>
2019-05-23 14:19:01 +0900

    secode commit

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ let moment = require('moment');
 
 var app =express();
 app.use(express.json());
-
+var now =moment().format("YYYY-MM-DD HH:mm:ss");
 app.post('/register',(req,res)=>{
 
   console.log("등록 구간 들어옴");
   console.log(req.body)
   if(req.body!=undefined){
 
-  var now =moment().format("YYYY-MM-DD HH:mm:ss");
+ 
   var user =[
     {
       "nickname":req.body.nickname,
@@ -60,10 +60,103 @@ app.post('/register',(req,res)=>{
 }
 
 });
-app.post('/login',(req,res)=>{
+app.post('/setpoint',(req,res)=>{
+  if(req.body._point!=undefined){
+  connection.query('update user set _point =_point+? where nickname = ?;',[req.body._point,req.body.nickname],(err,result)=>{
+    if(err){
+      console.log(err);
+      console.log("포인트 지급 오류");
+        res.send("포인트 지급 오류");
+    }else{
+      
+        console.log("포인트 지급 완료");
+        
+        res.send("포인트 지급 완료");
 
+     
+    }
+  
+  });
+}
 });
-
+app.post('/setkey',(req,res)=>{
+  if(req.body._key!=undefined){
+    connection.query('update user set _key =_key+? where nickname = ?;',[req.body._key,req.body.nickname],(err,result)=>{
+      if(err){
+        console.log(err);
+        console.log("키 지급 오류");
+          res.send("키 지급 오류");
+      }else{
+          console.log("키 지급 완료");
+          res.send("키 지급 완료");
+      }
+    });
+  }
+});
+app.post('/usepoint',(req,res)=>{
+  if(req.body._point!=undefined){
+    connection.query('update user set _point =_point-? where nickname = ?;',[req.body._point,req.body.nickname],(err,result)=>{
+      if(err){
+        console.log(err);
+        console.log("포인트 사용 오류");
+          res.send("포인트 사용 오류");
+      }else{
+          console.log("포인트 사용 완료");
+          res.send("포인트 사용 완료");
+      }
+    });
+  }
+});
+app.post('/usekey',(req,res)=>{
+  if(req.body._key!=undefined){
+    connection.query('update user set _key =_key-? where nickname = ?;',[req.body._key,req.body.nickname],(err,result)=>{
+      if(err){
+        console.log(err);
+        console.log("키 사용 오류");
+          res.send("키 사용 오류");
+      }else{
+          console.log("키 사용 완료");
+          res.send("키 사용 완료");
+      }
+    });
+  }
+});
+app.post('/get_topiclist',(req,res)=>{
+    connection.query('select * from topic',[],(err,result)=>{
+      if(err){
+        console.log(err);
+        console.log("topic리스트 가져오기 오류");
+          res.send("topic리스트 가져오기 오류");
+      }else{
+          console.log("topic리스트 가져오기 완료");
+          res.send(result);
+      }
+    });
+  
+});
+app.post('/register_topic',(req,res)=>{
+  if(req.body!=undefined){
+    var topic =[
+      {
+        "writer":req.body.writer,
+        "enroll_date":new Date(now),
+        "topic_content":req.body.topic_content,
+        "topic_title":req.body.topic_title,
+        "topic_category":req.body.topic_category
+      }
+    ];
+    connection.query('insert into topic set ?',topic,(err,result)=>{
+      if(err){
+        console.log(err);
+        console.log("topic 등록 오류");
+          res.send("topic 등록 오류");
+      }else{
+          console.log("topic 등록 성공");
+          res.send("topic 등록 성공");
+      }
+    });
+  }
+});
 app.listen(3000,()=>{
 console.log('Example app listening on port 3000!');
 });
